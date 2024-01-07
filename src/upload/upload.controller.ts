f5b316983504da8fe1087692450c9cff1ab0991d
kiraaziz <kiraaziz2020@gmail.com>
2024-01-07 18:30:14 +0100

    new

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,13 +1,6 @@
 import { Body, Controller, Head, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import * as sharp from 'sharp'
-import { initializeApp } from 'firebase-admin/app'
-import { getStorage } from 'firebase-admin/storage'
-
-process.env["FIREBASE_STORAGE_EMULATOR_HOST"] = "127.0.0.1:9199"
-initializeApp({ projectId: "lightai-2fc73" })
-
-const bucket = getStorage().bucket("lightai-2fc73.appspot.com")
 
 @Controller('upload')
 export class UploadController {
@@ -16,8 +9,7 @@ export class UploadController {
     @Post("/image")
     @UseInterceptors(FileInterceptor('image'))
     async uploadImage(@UploadedFile() file) {
-        const imageBuffer = sharp(file.buffer).resize(100)
-        await bucket.file('x.webp').save(imageBuffer)
+        const imageBuffer = sharp(file.buffer).toFormat("webp").resize(200)
         return imageBuffer
     }
 }
